8683d109569a809953d4522d721bb764005c2d4d
Daniel <danlpsarcanjo@gmail.com>
2021-08-28 16:58:22 -0400

    Add wall creation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
@@ -7,3 +7,13 @@ it('renders a 4x4 board', () => {
   const cells = screen.getAllByTestId('cell')
   expect(cells).toHaveLength(16);
 });
+
+it('can create wall on the board', () => {
+  render(<App />)
+  
+  const cell = screen.getAllByTestId('cell')[0]
+  fireEvent.click(cell)
+
+  const wall = screen.getByTestId('wall')
+  expect(wall).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [walls, setWalls] = useState<number[]>([])
+
   const board = [...Array(4)].map((_, i) => [...Array(4)].map((_, j) => i * 4 + j))
   return (
     <div className='h-screen w-screen flex justify-center items-center'>
@@ -8,8 +10,19 @@ function App() {
         {board.map((cells, i) => (
           <div className='flex gap-1' key={i}>
             {cells.map((number, j) => (
-              <div data-testid='cell' key={j} className='border border-black hover:bg-gray-200 cursor-pointer w-10 h-10 pl-1'>
-                {number}
+              <div data-testid='cell' key={j} className={'border border-black hover:bg-gray-200 cursor-pointer w-10 h-10 pl-1' + (walls.includes(number) ? ' bg-gray-700 text-gray-300' : '')}
+                onClick={() => {
+                  if (walls.includes(number)) {
+                    setWalls(walls.filter(w => w !== number))
+                    return
+                  }
+
+                  setWalls([...walls, number])
+                }}
+              >
+                <div data-testid={walls.includes(number) ? 'wall' : 'path'}>
+                  {number}
+                </div>
               </div>
             ))}
           </div>
