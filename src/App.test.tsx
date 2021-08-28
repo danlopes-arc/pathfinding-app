00ef038ca609391ebe186c75e535bbe37e683373
Daniel <danlpsarcanjo@gmail.com>
2021-08-28 16:20:12 -0400

    Create a 4x4 board

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders a 4x4 board', () => {
   render(<App />);
-  expect(true).toBeTruthy();
+  const cells = screen.getAllByTestId('cell')
+  expect(cells).toHaveLength(16);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function App() {
+  const board = [...Array(4)].map((_, i) => [...Array(4)].map((_, j) => i * 4 + j))
   return (
-    <h1>pathfinding</h1>
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <div className='flex flex-col gap-1'>
+        {board.map((cells, i) => (
+          <div className='flex gap-1' key={i}>
+            {cells.map((number, j) => (
+              <div data-testid='cell' key={j} className='border border-black hover:bg-gray-200 cursor-pointer w-10 h-10 pl-1'>
+                {number}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
