8a06bf8730630099fd9bdfedddb5393a4f347bac
Daniel <danlpsarcanjo@gmail.com>
2021-08-28 19:18:32 -0400

    Add end point

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -29,65 +29,82 @@ it('can revert a wall to a path', () => {
   expect(wall).not.toBeInTheDocument()
 })
 
-it('can add a starting point on the board', () => {
-  render(<App />)
+describe('starting point', () => {
+  it('can add a starting point on the board', () => {
+    render(<App />)
 
-  const addStartButton = screen.getByTestId('add-start-button')
-  fireEvent.click(addStartButton)
-  
-  const cell = screen.getAllByTestId('cell')[0]
-  fireEvent.click(cell)
+    const addStartButton = screen.getByTestId('add-start-button')
+    fireEvent.click(addStartButton)
+    
+    const cell = screen.getAllByTestId('cell')[0]
+    fireEvent.click(cell)
 
-  const start = screen.getByTestId('start')
-  expect(start).toBeInTheDocument()
+    const start = screen.getByTestId('start')
+    expect(start).toBeInTheDocument()
+  })
+
+  it('cannot add a staritng point on a wall', () => {
+    render(<App />)
+
+    const cell = screen.getAllByTestId('cell')[0]
+    fireEvent.click(cell)
+
+    const addStartButton = screen.getByTestId('add-start-button')
+    fireEvent.click(addStartButton)
+
+    fireEvent.click(cell)
+
+    const start = screen.queryByTestId('start')
+    expect(start).not.toBeInTheDocument()
+  })
+
+  it('goes back to wall mode after adding a starting point', () => {
+    render(<App />)
+
+    const addStartButton = screen.getByTestId('add-start-button')
+    fireEvent.click(addStartButton)
+    
+    const paths = screen.getAllByTestId('path')
+    const path0 = paths[0]
+    fireEvent.click(path0)
+
+    const path1 = paths[1]
+    fireEvent.click(path1)
+
+    const start = screen.getByTestId('start')
+    expect(start).toBe(path0)
+  })
+
+  it('switches start back to path when its clicked on', () => {
+    render(<App />)
+
+    const addStartButton = screen.getByTestId('add-start-button')
+    fireEvent.click(addStartButton)
+
+    const path = screen.getAllByTestId('path')[0]
+    fireEvent.click(path)
+    fireEvent.click(path)
+
+    const start = screen.queryByTestId('start')
+    expect(start).not.toBeInTheDocument()
+
+    const paths = screen.getAllByTestId('path')
+
+    expect(paths).toContain(path)
+  })
 })
 
-it('cannot add a staritng point on a wall', () => {
-  render(<App />)
+describe('end point', () => {
+  it('can add a end point on the board', () => {
+    render(<App />)
 
-  const cell = screen.getAllByTestId('cell')[0]
-  fireEvent.click(cell)
+    const addEndButton = screen.getByTestId('add-end-button')
+    fireEvent.click(addEndButton)
+    
+    const cell = screen.getAllByTestId('cell')[0]
+    fireEvent.click(cell)
 
-  const addStartButton = screen.getByTestId('add-start-button')
-  fireEvent.click(addStartButton)
-
-  fireEvent.click(cell)
-
-  const start = screen.queryByTestId('start')
-  expect(start).not.toBeInTheDocument()
-})
-
-it('goes back to wall mode after adding a starting point', () => {
-  render(<App />)
-
-  const addStartButton = screen.getByTestId('add-start-button')
-  fireEvent.click(addStartButton)
-  
-  const paths = screen.getAllByTestId('path')
-  const path0 = paths[0]
-  fireEvent.click(path0)
-
-  const path1 = paths[1]
-  fireEvent.click(path1)
-
-  const start = screen.getByTestId('start')
-  expect(start).toBe(path0)
-})
-
-it('switches start back to path when its clicked on', () => {
-  render(<App />)
-
-  const addStartButton = screen.getByTestId('add-start-button')
-  fireEvent.click(addStartButton)
-
-  const path = screen.getAllByTestId('path')[0]
-  fireEvent.click(path)
-  fireEvent.click(path)
-
-  const start = screen.queryByTestId('start')
-  expect(start).not.toBeInTheDocument()
-
-  const paths = screen.getAllByTestId('path')
-
-  expect(paths).toContain(path)
+    const end = screen.getByTestId('end')
+    expect(end).toBeInTheDocument()
+  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
-type GameMode = 'idle' | 'start'
+type GameMode = 'idle' | 'start' | 'end'
 
 function App() {
   const [walls, setWalls] = useState<number[]>([])
   const [mode, setMode] = useState<GameMode>('idle')
   const [startingCell, setStartingCell] = useState<number | null>(null)
+  const [endCell, setEndCell] = useState<number | null>(null)
 
   const board = [...Array(4)].map((_, i) => [...Array(4)].map((_, j) => i * 4 + j))
 
   const wallClasses = 'bg-gray-700 hover:bg-gray-800 text-gray-300'
   const pathClasses = 'hover:bg-gray-200'
   const startClasses = 'bg-green-500 hover:bg-green-700 hover:text-gray-200'
+  const endClasses = 'bg-blue-500 hover:bg-blue-700 hover:text-gray-200'
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
@@ -19,7 +21,13 @@ function App() {
         {board.map((cells, i) => (
           <div className='flex gap-1' key={i}>
             {cells.map((number, j) => (
-              <div data-testid='cell' key={j} className={'border border-black cursor-pointer w-10 h-10 pl-1 ' + (walls.includes(number) ? wallClasses : (startingCell === number ? startClasses : pathClasses))}
+              <div
+                data-testid='cell'
+                key={j}
+                className={'border border-black cursor-pointer w-10 h-10 pl-1 ' + 
+                  (walls.includes(number) ? wallClasses :
+                    (startingCell === number ? startClasses :
+                      (endCell === number ? endClasses : pathClasses)))}
                 onClick={() => {
                   if (mode === 'start') {
                     if (walls.includes(number)) {
@@ -31,7 +39,22 @@ function App() {
                     return
                   }
 
+                  if (mode === 'end') {
+                    if (walls.includes(number)) {
+                      setMode('idle')
+                      return
+                    }
+                    setEndCell(number)
+                    setMode('idle')
+                    return
+                  }
+
                   if (startingCell === number) {
+                    setStartingCell(null)
+                    return
+                  }
+
+                  if (endCell === number) {
                     setStartingCell(null)
                     return
                   }
@@ -44,7 +67,7 @@ function App() {
                   setWalls([...walls, number])
                 }}
               >
-                <div data-testid={walls.includes(number) ? 'wall' : (startingCell === number ? 'start' : 'path')}>
+                <div data-testid={walls.includes(number) ? 'wall' : (startingCell === number ? 'start' : (endCell === number ? 'end' : 'path'))}>
                   {number}
                 </div>
               </div>
@@ -62,6 +85,17 @@ function App() {
         className={mode === 'start'? 'bg-green-400' : ''}
       >
         Add starting point
+      </button>
+      <button data-testid='add-end-button' onClick={() => {
+        if (mode === 'idle') {
+          setMode('end')
+          return
+        }
+        setMode('idle')
+      }}
+        className={mode === 'end'? 'bg-blue-400' : ''}
+      >
+        Add end point
       </button>
     </div>
   );
